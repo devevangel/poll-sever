bbb446400a6e7e8c37d1b8455bb36fc4be89a941
ChidiBrokeDNet <59733962+devevangel@users.noreply.github.com>
2022-11-12 03:29:14 +0000

    Added DB

--- routes/voteRoute.js
+++ routes/voteRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const voteController = require("../Controllers/voteController");
-const userController = require("../Controllers/userController");
+const voteController = require("../controllers/voteController");
+const userController = require("../controllers/userController");
 
 const router = express.Router();
 
